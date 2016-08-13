ed5f69f9b7135fc28697e2b839b68c2b82821d3a
Nadav Barami <nadavbara@gmail.com>
2016-08-13 15:05:56 +0300

    checked github status code

--- index.android.js
+++ index.android.js
@@ -143,7 +143,7 @@ export default class Example extends Component {
   'Alert Title',
   'My Alert Msg',
   [
-    {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+    {text: 'Ask me in two days', onPress: () => console.log('Ask me later pressed')},
     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
     {text: 'OK', onPress: () => console.log('OK Pressed')},
   ]
